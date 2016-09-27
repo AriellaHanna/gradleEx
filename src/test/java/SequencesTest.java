23662664edf63274d56d0651bf78957713b33b95
ard71 <ard71@pitt.edu>
2016-09-27 13:47:00 -0400

    We did things

--- src/test/java/SequencesTest.java
+++ src/test/java/SequencesTest.java
@@ -1,1 +1,35 @@
-
+import org.junit.Test;
+import static org.junit.Assert.*;
+import ard71.Sequences;
+public class SequencesTest
+{
+	@Test
+	public void testTri1() {
+		assertEquals(Sequences.tri(1), 1);
+	}
+	
+	@Test
+	public void testTri2() {
+		assertEquals(Sequences.tri(2), 3);
+	}
+	
+	@Test
+	public void testTri4() {
+		assertEquals(Sequences.tri(4), 10);
+	}
+	
+	@Test
+	public void testLazy0() {
+		assertEquals(Sequences.lazy(0), 1);
+	}
+	
+	@Test
+	public void testLazy3() {
+		assertEquals(Sequences.lazy(3), 7);
+	}
+	
+	@Test
+	public void testLazy5() {
+		assertEquals(Sequences.lazy(5), 16);
+	}
+}
